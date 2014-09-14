6d840e0c3bcaee7e10c0ae40b11133106832b339
Alexander Bragdon <alexbragdon@me.com>
2014-09-14 17:37:21 -0400

    Adds the setUp hands method to the AlphaHantoGame

--- src/hanto/studentACBSJH/alpha/AlphaHantoGame.java
+++ src/hanto/studentACBSJH/alpha/AlphaHantoGame.java
@@ -30,9 +30,6 @@ public class AlphaHantoGame extends BaseHantoGame implements HantoGame {
 	@Override
 	public MoveResult makeMove(HantoPieceType pieceType, HantoCoordinate from,
 			HantoCoordinate to) throws HantoException {
-		HantoPieceACBSJH newPiece = new HantoPieceACBSJH(getCurrentPlayersTurn());
-		
-		HantoPieces.add(newPiece);
 		TurnNumber++;
 		
 		return MoveResult.OK;
@@ -53,6 +50,18 @@ public class AlphaHantoGame extends BaseHantoGame implements HantoGame {
 	public String getPrintableBoard() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see hanto.studentACBSJH.common.BaseHantoGame#setupHands()
+	 */
+	@Override
+	protected void setupHands() {
+		HantoPieceACBSJH blueButterfly = new HantoPieceACBSJH(HantoPlayerColor.BLUE);
+		HantoPieces.add(blueButterfly);
+		
+		HantoPieceACBSJH redButterfly = new HantoPieceACBSJH(HantoPlayerColor.RED);
+		HantoPieces.add(redButterfly);
 	}
 
 }
