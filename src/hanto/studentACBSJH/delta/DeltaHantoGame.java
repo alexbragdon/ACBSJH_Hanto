723ceafdd725fc6ec6a9c93c75c842383901b5da
Alexander Bragdon <alexbragdon@me.com>
2014-10-01 21:33:44 -0400

    Adds DeltaHantoGame

---
+++ src/hanto/studentACBSJH/delta/DeltaHantoGame.java
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Sean J. Halloran & Alexander C. Bragdon
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *		Sean Halloran
+ *		Alex Bragdon
+ *******************************************************************************/
+package hanto.studentACBSJH.delta;
+
+import hanto.common.HantoCoordinate;
+import hanto.common.HantoException;
+import hanto.common.HantoGame;
+import hanto.common.HantoGameID;
+import hanto.common.HantoPieceType;
+import hanto.common.HantoPlayerColor;
+import hanto.common.MoveResult;
+import hanto.studentACBSJH.common.BaseHantoGame;
+
+/**
+ * @author alexbragdon
+ *
+ */
+public class DeltaHantoGame extends BaseHantoGame implements HantoGame {
+
+	/**
+	 * @param gameID
+	 * @param firstPlayerColor
+	 */
+	public DeltaHantoGame(HantoGameID gameID, HantoPlayerColor firstPlayerColor) {
+		super(gameID, firstPlayerColor);
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see hanto.common.HantoGame#makeMove(hanto.common.HantoPieceType, hanto.common.HantoCoordinate, hanto.common.HantoCoordinate)
+	 */
+	@Override
+	public MoveResult makeMove(HantoPieceType pieceType, HantoCoordinate from,
+			HantoCoordinate to) throws HantoException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see hanto.studentACBSJH.common.BaseHantoGame#setupHands()
+	 */
+	@Override
+	protected void setupHands() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
