e822323ef4631c1851903e014db66ced79b9387f
Sean Halloran <sean@myomo.com>
2014-10-14 13:24:42 -0400

    adds interface for last commit, woops!

---
+++ test/common/ImpossibleHantoTestGame.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Sean J. Halloran
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *		Sean Halloran
+ *******************************************************************************/
+package common;
+
+import hanto.common.HantoPieceType;
+import hanto.common.HantoPlayerColor;
+
+import java.util.Collection;
+
+/**
+ * @author Sean
+ *
+ */
+public interface ImpossibleHantoTestGame extends HantoTestGame {
+	
+	public void clearPieces();
+	
+	public void addPiecesToHand(HantoPieceType[] newPieces, HantoPlayerColor player);
+
+}
